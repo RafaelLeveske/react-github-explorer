56680b7e5171f9af933e2ab1fcd6187f1ba3c1e9
Rafael Vieira <rafaelleveske@gmail.com>
2020-07-28 22:55:57 -0300

    Estilização da pagina Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => (
-  <Title>Explore repositórios no Github.</Title>
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github.</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/61164438?s=460&u=adeab63e92596927174349d3227ed3613f40f1cd&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>RafaelLeveske/github-explorer-frontend</strong>
+          <p>
+            Projeto front end em ReactJs referente ao modulo 3 do bootcamp
+            GoStack 12.
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/61164438?s=460&u=adeab63e92596927174349d3227ed3613f40f1cd&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>RafaelLeveske/github-explorer-frontend</strong>
+          <p>
+            Projeto front end em ReactJs referente ao modulo 3 do bootcamp
+            GoStack 12.
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/61164438?s=460&u=adeab63e92596927174349d3227ed3613f40f1cd&v=4"
+          alt="Avatar"
+        />
+        <div>
+          <strong>RafaelLeveske/github-explorer-frontend</strong>
+          <p>
+            Projeto front end em ReactJs referente ao modulo 3 do bootcamp
+            GoStack 12.
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
 );
 
 export default Dashboard;
